a687ebe5539f49a0ef59c99f06028e400fb50a10
Your Name <you@example.com>
2019-06-28 21:44:57 +0545

    freecodecamp, posts, following, followed & background color

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,19 +2,23 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fktm7-1.fna.fbcdn.net/vp/2041d815426b6d7d2c8cb94837574812/5DB47138/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fktm7-1.fna.fbcdn.net" class="rounded-circle ">
+        </div>
+        <div class="col-9 pt-5">
+            <div>
+                <h1>freecodecamp</h1>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5">
+                    <strong>153</strong> posts
+                </div>
+                <div class="pr-5">
+                    <strong>23k</strong> followers
+                </div>
+                <div class="pr-5">
+                    <strong>212</strong> following
                 </div>
             </div>
         </div>
