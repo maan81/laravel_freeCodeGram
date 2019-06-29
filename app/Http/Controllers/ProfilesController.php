67f596bac2cf3c15d1eca7691274c7fb704b46d8
Your Name <you@example.com>
2019-06-29 13:34:30 +0545

    profile controller pass id & retrieve data & display in view

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
@@ -11,8 +12,10 @@ class ProfilesController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index($user)
     {
-        return view('home');
+        $user = User::find($user);
+
+        return view('home', ['user' => $user]);
     }
 }
