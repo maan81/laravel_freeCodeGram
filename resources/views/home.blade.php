3b8cfcde45675a6bf0262269479ec864eafdb86c
Your Name <you@example.com>
2019-06-29 14:31:31 +0545

    show obtained data in view * N/A if not present

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,12 +21,10 @@
                     <strong>212</strong> following
                 </div>
             </div>
-            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
             <div>
-                We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.
-            </div>
-            <div>
-                <a href="#">www.freecodecamp.org</a>
+                <a href="#">{{ $user->profile->url ?? 'N/A' }}</a>
             </div>
         </div>
     </div>
