c669fd88c55678e2e6f9e226707d026b0e60893b
Your Name <you@example.com>
2019-06-29 21:08:58 +0545

    aligning ProfileController

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -17,6 +17,8 @@ class ProfilesController extends Controller
         // $user = User::find($user);
         $user = User::findOrFail($user);
 
-        return view('home', ['user' => $user]);
+        return view('profiles.index', [
+            'user' => $user
+        ]);
     }
 }
