beba0e67c6d2423dd122e6a5f8b0fdfca08537e8
Your Name <you@example.com>
2019-07-03 12:50:25 +0545

    showing $post->user->username & caption

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,8 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="col-8">
-        <img class="w-100" src="/storage/ {{ $post->image }}" alt="{{ $post->caption }}">
+    <div class="row">
+        <div class="col-8">
+            <img class="w-100" src="/storage/{{ $post->image }}" alt="{{ $post->caption }}">
+        </div>
+
+        <div class="col-4">
+            <div>
+                <h3>{{ $post->user->username }}</h3>
+
+                <p>{{ $post->caption }}</p>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
