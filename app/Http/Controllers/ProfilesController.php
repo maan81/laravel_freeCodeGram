b35275d2d62e58870292bba6539b1f2aff91686a
Your Name <you@example.com>
2019-07-03 22:32:59 +0545

    restricting editing data to edit the users data ..... not successful !!!!!

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -34,7 +34,7 @@ class ProfilesController extends Controller
             'image' => '',
         ]);
 
-        $user->profile->update($data);
+        auth()->user()->profile->update($data);
 
         return redirect("/profile/{$user->id}");
     }
