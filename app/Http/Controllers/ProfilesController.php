712706d0f21ca1df33680b841f7e578c78aae0de
Your Name <you@example.com>
2019-07-03 22:12:49 +0545

    validating profile data

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -26,4 +26,14 @@ class ProfilesController extends Controller
     {
         return view('profiles.edit', compact('user'));
     }
+
+    public function update(User $user)
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'url' => 'url',
+            'image' => '',
+        ]);
+    }
 }
