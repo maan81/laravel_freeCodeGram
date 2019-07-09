cd2919141afda40a77f7cd6d56711f89ecc7e545
Your Name <you@example.com>
2019-07-09 12:54:30 +0545

    new user registration email template

--- resources/views/emails/welcome-email.blade.php
+++ resources/views/emails/welcome-email.blade.php
@@ -1,12 +1,8 @@
 @component('mail::message')
-# Introduction
+# Welcome to freeCodeGram
 
-The body of your message.
+This is a community of follow developers and we love that you have joined us.
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
+All the best,<br>
+maan.
 @endcomponent
