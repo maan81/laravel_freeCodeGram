ba67996b80bca6fa52c5b49bf5436302ab4cb3b4
maan81 <pranijman@gmail.com>
2019-07-11 16:58:14 +0545

    Refactoring posts.show to include Posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -64,7 +64,7 @@ class PostsController extends Controller
         // dd(request()->all());
     }
 
-    public function show(\App\Post $post)
+    public function show(Post $post)
     {
         return view('posts.show', compact('post'));
     }
